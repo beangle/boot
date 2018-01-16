b6b3be6403614e027fb6b537124f415337fdfd2b
段体华 <duantihua@gmail.com>
2018-01-16 15:04:43 +0800

    first commit

---
+++ artifact/src/test/scala/org/beangle/repo/artifact/RepoTest.scala
@@ -0,0 +1,36 @@
+/*
+ * Beangle, Agile Development Scaffold and Toolkit
+ *
+ * Copyright (c) 2005-2018, Beangle Software.
+ *
+ * Beangle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beangle is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Beangle.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beangle.repo.artifact
+
+import org.junit.runner.RunWith
+import org.scalatest.{ FunSpec, Matchers }
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class RepoTest extends FunSpec with Matchers {
+
+  describe("Local repo") {
+    it("to string") {
+      val local = Repo.local(null);
+      val artifact = Artifact("org.beangle.commons:beangle-commons-web_2.12:5.0.0.M6")
+      println(local.lastestBefore(artifact))
+      println(local.lastest(artifact))
+    }
+  }
+}
