ee17428f869c136baf2c6eef45d8d70fa1250d00
段体华 <duantihua@gmail.com>
2021-08-25 23:42:33 +0800

    update version in scripts

--- src/main/scripts/launch.sh
+++ src/main/scripts/launch.sh
@@ -84,19 +84,18 @@ detect_jarfile(){
   fi
 }
 
-export scala_ver=2.13.5
-export beangle_commons_ver=5.2.2
-export slf4j_ver=2.0.0-alpha1
-export logback_ver=1.3.0-alpha5
-export commons_compress_ver=1.19
-export boot_ver=0.0.23
+export scala_ver=2.13.6
+export beangle_commons_ver=5.2.5
+export slf4j_ver=2.0.0-alpha4
+export logback_ver=1.3.0-alpha9
+export commons_compress_ver=1.21
+export boot_ver=0.0.24
 
 download org.scala-lang scala-library $scala_ver
-download org.scala-lang scala-reflect $scala_ver
-download org.beangle.commons beangle-commons-core_2.13  $beangle_commons_ver
-download org.beangle.commons beangle-commons-file_2.13  $beangle_commons_ver
+download org.beangle.commons beangle-commons-core_3  $beangle_commons_ver
+download org.beangle.commons beangle-commons-file_3  $beangle_commons_ver
 download org.apache.commons commons-compress $commons_compress_ver
-download org.beangle.boot beangle-boot_2.13 $boot_ver
+download org.beangle.boot beangle-boot_3 $boot_ver
 download org.slf4j slf4j-api $slf4j_ver
 download ch.qos.logback logback-core $logback_ver
 download ch.qos.logback logback-classic $logback_ver
