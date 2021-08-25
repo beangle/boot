715b63cae5b2e2b0eef2174241110114688a0bd3
段体华 <duantihua@gmail.com>
2021-08-26 00:06:05 +0800

    add scala3-library

--- src/main/scripts/launch.sh
+++ src/main/scripts/launch.sh
@@ -84,14 +84,17 @@ detect_jarfile(){
   fi
 }
 
-export scala_ver=2.13.6
+export scala_ver=3.0.1
+export scala_lib_ver=2.13.6
+
 export beangle_commons_ver=5.2.5
 export slf4j_ver=2.0.0-alpha4
 export logback_ver=1.3.0-alpha9
 export commons_compress_ver=1.21
 export boot_ver=0.0.24
 
-download org.scala-lang scala-library $scala_ver
+download org.scala-lang scala3-library_3 $scala_ver
+download org.scala-lang scala-library $scala_lib_ver
 download org.beangle.commons beangle-commons-core_3  $beangle_commons_ver
 download org.beangle.commons beangle-commons-file_3  $beangle_commons_ver
 download org.apache.commons commons-compress $commons_compress_ver
@@ -107,6 +110,7 @@ args="${opts#*$jarfile}"
 options="${opts%%$jarfile*}"
 java -cp "${bootpath:1}" org.beangle.boot.dependency.AppResolver $jarfile $M2_REMOTE_REPO $M2_REPO
 info=`java -cp "${bootpath:1}" org.beangle.boot.launcher.Classpath $jarfile $M2_REPO`
+
 if [ $? = 0 ]; then
   mainclass="${info%@*}"
   classpath="${info#*@}"
